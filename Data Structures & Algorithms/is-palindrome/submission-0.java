306d5d2862799dee949057ef8cc4496d9bf72327
roshanchauhan08 <roshanrcc233@gmail.com>
2026-09-17 09:51:43 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder newS= new StringBuilder();
+        for(char c:s.toCharArray()){
+          if(Character.isLetterOrDigit(c)){
+            newS.append(Character.toLowerCase(c));
+          }
+        }
+        return newS.toString().equals(newS.reverse().toString());
+    }
+}
